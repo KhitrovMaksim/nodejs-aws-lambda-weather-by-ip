28af6f016571d092c4ed354d5782084c868caf6f
maksim khitrov <khitrov.maks@gmail.com>
2023-07-14 20:08:59 +0400

    Added lambda function

--- config.mjs
+++ config.mjs
@@ -1,1 +1,2 @@
-export const TOKEN_WEATHER_API = process.env.OPEN_WEATHER_MAP_TOKEN;
+export const TOKEN_WEATHER_API = process.env.OPEN_WEATHER_MAP_TOKEN,
+  IP_STACK_TOKEN = process.env.IP_STACK_TOKEN;
--- index.mjs
+++ index.mjs
@@ -1,18 +1,34 @@
 import { httpsGetJson } from './lib/httpsGetJson.mjs';
+import { httpGetJson } from './lib/httpGetJson.mjs';
+import { ipStackApiUrlBuilder } from './lib/ipStackApiUrlBuilder.mjs';
+import { ipStackApiResponseParser } from './lib/ipStackApiResponseParser.mjs';
 import { openWeatherMapApiUrlBuilder } from './lib/openWeatherMapApiUrlBuilder.mjs';
 import { openWeatherMapApiResponseParser } from './lib/openWeatherMapApiResponseParser.mjs';
-import { TOKEN_WEATHER_API } from './config.mjs';
+import { getHtmlWeatherByIp } from './lib/getHtmlWeatherByIp.mjs';
+import { TOKEN_WEATHER_API, IP_STACK_TOKEN } from './config.mjs';
 
-export const handler = async (event) => {
-  const url = openWeatherMapApiUrlBuilder(TOKEN_WEATHER_API, '40.16557', '44.29462');
+export const handler = async (event, context, callback) => {
+  try {
+    const ip = event['requestContext']['http']['sourceIp'];
 
-  const json = await httpsGetJson(url);
+    const urlIpApi = ipStackApiUrlBuilder(IP_STACK_TOKEN, ip);
+    const jsonIpApi = await httpGetJson(urlIpApi);
+    const { latitude, longitude } = ipStackApiResponseParser(jsonIpApi);
 
-  const parsedJson = openWeatherMapApiResponseParser(json);
+    const urlWeatherApi = openWeatherMapApiUrlBuilder(TOKEN_WEATHER_API, latitude, longitude);
+    const jsonWeatherApi = await httpsGetJson(urlWeatherApi);
+    const parsedJsonWeatherApi = openWeatherMapApiResponseParser(jsonWeatherApi);
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(parsedJson),
-  };
-  return response;
+    const content = getHtmlWeatherByIp(parsedJsonWeatherApi);
+
+    const response = {
+      statusCode: 200,
+      headers: { 'Content-Type': 'text/html' },
+      body: content,
+    };
+
+    return response;
+  } catch (error) {
+    return JSON.stringify(error);
+  }
 };
