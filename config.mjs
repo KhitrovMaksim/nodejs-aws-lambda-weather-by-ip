6cdbe956f9877b25a4b395720cbe045374e0c151
maksim khitrov <khitrov.maks@gmail.com>
2023-07-14 16:00:33 +0400

    Added weather forcast, token, env and hardcoded location

---
+++ config.mjs
@@ -0,0 +1,1 @@
+export const TOKEN_WEATHER_API = process.env.OPEN_WEATHER_MAP_TOKEN;
--- index.mjs
+++ index.mjs
@@ -1,8 +1,18 @@
+import { httpsGetJson } from './lib/httpsGetJson.mjs';
+import { openWeatherMapApiUrlBuilder } from './lib/openWeatherMapApiUrlBuilder.mjs';
+import { openWeatherMapApiResponseParser } from './lib/openWeatherMapApiResponseParser.mjs';
+import { TOKEN_WEATHER_API } from './config.mjs';
+
 export const handler = async (event) => {
-  // TODO implement
+  const url = openWeatherMapApiUrlBuilder(TOKEN_WEATHER_API, '40.16557', '44.29462');
+
+  const json = await httpsGetJson(url);
+
+  const parsedJson = openWeatherMapApiResponseParser(json);
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify(parsedJson),
   };
   return response;
 };
